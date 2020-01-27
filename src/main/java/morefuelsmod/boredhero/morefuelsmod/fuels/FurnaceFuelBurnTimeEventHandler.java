ae9ae6cc95c080aeb349b89a285be1de258bb427
Noah Martino <personal.boredhero@gmail.com>
2020-01-27 05:12:02 -0500

    An attempt was made

---
+++ src/main/java/morefuelsmod/boredhero/morefuelsmod/fuels/FurnaceFuelBurnTimeEventHandler.java
@@ -0,0 +1,24 @@
+//https://github.com/TheGreyGhost/MinecraftByExample/blob/master/src/main/java/minecraftbyexample/mbe35_recipes/FurnaceFuelBurnTimeEventHandler.java
+
+package morefuelsmod.boredhero.morefuelsmod.fuels;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class FurnaceFuelBurnTimeEventHandler {
+	public static final FurnaceFuelBurnTimeEventHandler instance = new FurnaceFuelBurnTimeEventHandler();
+	
+	private FurnaceFuelBurnTimeEventHandler() {
+		@SubscribeEvent
+		public void onFurnaceFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event) {
+			ItemStack fuel = event.getItemStack();
+			final int BURN_TIME_SECONDS = 5;
+			final int TICKS_PER_SECOND = 20;
+			if(fuel.getItem() == Items.WHEAT) {
+				event.setBurnTime(BURN_TIME_SECONDS * TICKS_PER_SECOND);
+			}
+		}
+	}
+}
