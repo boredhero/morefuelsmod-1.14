6d285298f13f2be37b3060fd4a3e9c5610fd4186
Noah Martino <personal.boredhero@gmail.com>
2020-01-28 00:40:25 -0500

    Cleaned up main class

--- src/main/java/morefuelsmod/boredhero/morefuelsmod/MoreFuelsMod.java
+++ src/main/java/morefuelsmod/boredhero/morefuelsmod/MoreFuelsMod.java
@@ -15,50 +15,35 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-//TODO: BEWARE THAT THIS MAIN CLASS IS CLUTTERED AND IS A COPY - PASTE - MODIFY OF THE 1.14 EXAMPLEMOD.JAVA
-//MANY THINGS THAT LOOK LIKE THEY ARE MISSING HERE ARE INSTEAD TAKEN CARE OF IN MFMEventSubscriber.java
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(MoreFuelsMod.modid)
 public class MoreFuelsMod {
 
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
-    //This isn't really correct but I'm not sure how exactly we do this with 1.14 so until i find out more i'll use this when printing to the terminal during init stages.
     public static final String version = "1.7.0";
     public static final String mcversion = "1.14.4";
     public static final String modid = "morefuelsmod";
 
     public MoreFuelsMod() {
-    	//Config stuff
     	ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, MoreFuelsModConfig.clientSpec);
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(MFMFuelsEventHandler.instance);
     }
     private void setup(final FMLCommonSetupEvent event)
     {
-        LOGGER.info("Loading morefuelsmod-" + mcversion + " " + version);
-        LOGGER.info("morefuelsmod: Lets Load some F*****G ores");
+        LOGGER.info("Loading morefuelsmod-" + mcversion + " v" + version);
+        LOGGER.info("MFM: Lets Load some F*****G ores");
         MFMOreGen.setupOreGenPart1();
-        LOGGER.info("F*****G ores loaded");
+        LOGGER.info("MFM: F*****G ores loaded");
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("Server instance detected. Ditching the clientside.");
-        MFMOreGen.setupOreGenPart1();
+        LOGGER.info("MFM: Server Instance Detected");
+        //MFMOreGen.setupOreGenPart1(); Had this here before, but it still didn't do anything useful.
     }
 }
