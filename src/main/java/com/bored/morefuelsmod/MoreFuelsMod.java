5e2dc9ecc3a035537b439384ee1f5a9e90609627
Noah Martino <personal.boredhero@gmail.com>
2020-09-22 18:00:29 -0400

    Formally update version

--- src/main/java/com/bored/morefuelsmod/MoreFuelsMod.java
+++ src/main/java/com/bored/morefuelsmod/MoreFuelsMod.java
@@ -101,7 +101,7 @@ public class MoreFuelsMod {
 
 	public static final String modId = "morefuelsmod";
 	public static final String name = "More Fuels Mod";
-	public static final String version = "1.6.2";
+	public static final String version = "1.6.2-HOTFIX-1";
 
 	@Mod.Instance(modId)
 	public static MoreFuelsMod instance;
